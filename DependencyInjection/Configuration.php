457e7c94f868d685c3ba7bff9accd21b0f49730a
rudak <roiduboul@gmail.com>
2015-01-23 17:03:00 +0100

    add params

---
+++ DependencyInjection/Configuration.php
@@ -0,0 +1,23 @@
+<?php
+namespace Rudak\ContactBundle\DependencyInjection;
+
+use Symfony\Component\Config\Definition\Builder\TreeBuilder;
+use Symfony\Component\Config\Definition\ConfigurationInterface;
+
+class Configuration implements ConfigurationInterface
+{
+    public function getConfigTreeBuilder()
+    {
+        $treeBuilder = new TreeBuilder();
+        $rootNode = $treeBuilder->root('rudak_contact');
+
+        $rootNode
+            ->children()
+            ->scalarNode('email_from')->defaultValue('from@email.fr')->end()
+            ->scalarNode('email_to')->defaultValue('to@email.fr')->end()
+            ->end()
+        ;
+
+        return $treeBuilder;
+    }
+}
--- README.md
+++ README.md
@@ -14,3 +14,9 @@ ajouter dans routing.yml
         rudak_contact:
             resource: "@RudakContactBundle/Resources/config/routing.yml"
             prefix:   /
+
+ajouter la config qui va bien dans app/config.yml
+
+        rudak_contact:
+            email_from: email-from@email.fr
+            email_to : email-to@email.fr
